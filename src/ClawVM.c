3fa10864df2bb478a2e42a1af2f8bd274f6268b7
Benedikt Müssig <benedikt.muessig@gmail.com>
2015-11-10 20:19:52 +0100

    Initial Commit

---
+++ src/ClawVM.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : ClawVM.c
+ Author      : ninjabyte electronics
+ Version     :
+ Copyright   : (c) 2015 ninjabyte electronics
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello World!"); /* prints Hello World! */
+	return EXIT_SUCCESS;
+}
