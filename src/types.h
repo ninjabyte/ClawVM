061c7c02b2470035a1ac461a1682576833f4bfc4
Benedikt Müssig <benedikt.muessig@gmail.com>
2015-11-23 20:42:25 +0100

    Added types and vmstate

---
+++ src/types.h
@@ -0,0 +1,20 @@
+/*
+ * types.h
+ *
+ *  Created on: 23.11.2015
+ *      Author: Benedikt
+ */
+
+#ifndef TYPES_H_
+#define TYPES_H_
+
+#include <stdint.h>
+
+/* Claw types */
+typedef int16_t var_t; /* Variable */
+typedef int8_t* array_t; /* Array pointer */
+
+/* VM state variables */
+typedef int32_t pcptr_t; /* Program counter pointer */
+
+#endif /* TYPES_H_ */
---
+++ src/vmstate.h
@@ -0,0 +1,23 @@
+/*
+ * vmstate.h
+ *
+ *  Created on: 23.11.2015
+ *      Author: Benedikt
+ */
+
+#ifndef VMSTATE_H_
+#define VMSTATE_H_
+
+#include "types.h"
+
+#ifndef STACK_SIZE
+#define STACK_SIZE 512
+#endif
+
+typedef struct {
+	FILE* file; /* Input file */
+	pcptr_t pc; /* Program counter */
+	var_t stack[STACK_SIZE]; /* Global stack */
+} vmstate_t;
+
+#endif /* VMSTATE_H_ */
