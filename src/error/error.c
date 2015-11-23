ecbedee13cd9cdd88ec41fbd900ea51a5a2f82d1
Benedikt Müssig <benedikt.muessig@gmail.com>
2015-11-23 21:44:15 +0100

    Added new errors

--- src/error/error.c
+++ src/error/error.c
@@ -7,6 +7,13 @@ const char* error_messages[] =
 	"No errors occurred",
 	"Invalid file",
 	"HELP Im on fire D:",
+	"Unimplemented instruction",
+
+	/* Runtime errors */
+	"Division by zero",
+	"Variable is of invalid type",
+	"Variable is invalid or does not exist",
+	"Jump out of bounds"
 };
 
 /* prints the error message of an error code */
